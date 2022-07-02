2a3048aea90943139f8ace404ffc1e6c5c998f98
unknown <nikolakostadinov@protonmail.com>
2022-07-02 15:46:16 +0300

    ne stane

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM python:3.10
 
 WORKDIR /app
 
-RUN pip install json argparse subprocess
-
 COPY . .
 
-CMD [ "python", "./main.py" ]
+RUN pip install --upgrade pip
+
+RUN pip install -r ./requirements.txt
+
+CMD [ "python", "main.py" ]
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import json
 import subprocess
+import os
 
+# Get Input
 command = input().split(' ')
 mainCommand = command[0]
 commandArgument = command[1]
 
+# Open Commands JSON
 jsonFilePath = './commands.json'
 assistantCommands = json.load(open(jsonFilePath))
 
+# Run Script
 try:
     
     scriptPath = f'scripts/{ assistantCommands[mainCommand] }'
     
-except KeyError: print('COMMAND IS SHIT')
+except KeyError: raise('COMMAND IS NOT DEFINED')
 
 subprocess.run(f'python { scriptPath } --file { commandArgument }', shell=True)
